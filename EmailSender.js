bbf2ab47d6e899db93c09051be968e72ec9b97b0
Manasvi Sripada <85676177+20WH5A1203-Manasvi@users.noreply.github.com>
2021-12-04 23:34:19 +0530

    Update EmailSender.js

--- EmailSender.js
+++ EmailSender.js
@@ -1,8 +1,8 @@
 // include nodemailer
 const nodemailer = require('nodemailer');
 // declare vars
-let fromMail = 'keerthi.kanuganti74@gmail.com';
-let toMail = 'smileybhargavi2000@gmail.com';
+let fromMail = 'sendername@gmail.com';
+let toMail = 'receivername@gmail.com';
 
 let subject  = 'An email using nodejs app';
 let text = "My name is Keerthi,Thank you for Helping me!!" 
@@ -11,8 +11,8 @@ let text = "My name is Keerthi,Thank you for Helping me!!"
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'keerthi.kanuganti74@gmail.com',
-        pass: '8790424546'
+        user: 'sendername@gmail.com',
+        pass: '******'
     }
 });
 
